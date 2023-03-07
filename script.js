92bf022e2ba1281bc7073f6ffb37973842da7fc5
Luan Rezende <flogsgameluan@gmail.com>
2023-03-07 02:54:33 -0300

    new version

--- script.js
+++ script.js
@@ -1,17 +1,67 @@
-const noticiasLista = document.getElementById("noticias-lista");
+// Seção de política
+const politicaLista = document.getElementById("politica-lista");
 
-fetch( // SE PREFERIR PT-BR, MUDE NA URL "country=us" para "country=br"
-  "https://newsapi.org/v2/top-headlines?country=us&category=sport&apiKey=SUA_API_AQUI"
-)
-  .then((response) => response.json())
-  .then((data) => {
-    data.articles.forEach((article) => {
-      const li = document.createElement("li");
-      const link = document.createElement("a");
-      link.href = article.url;
-      link.textContent = article.title;
-      li.appendChild(link);
-      noticiasLista.appendChild(li);
-    });
-  })
-  .catch((error) => console.log(error));
+fetch("https://newsapi.org/v2/top-headlines?country=br&category=politics&apiKey=SUA_API_KEY")
+	.then(response => response.json())
+	.then(data => {
+		data.articles.forEach(article => {
+			const li = document.createElement("li");
+			const link = document.createElement("a");
+			link.href = article.url;
+			link.textContent = article.title;
+			li.appendChild(link);
+			politicaLista.appendChild(li);
+		});
+	})
+	.catch(error => console.log(error));
+
+// Seção de esportes
+const esportesLista = document.getElementById("esportes-lista");
+
+fetch("https://newsapi.org/v2/top-headlines?country=br&category=sports&apiKey=SUA_API_KEY")
+	.then(response => response.json())
+	.then(data => {
+		data.articles.forEach(article => {
+			const li = document.createElement("li");
+			const link = document.createElement("a");
+			link.href = article.url;
+			link.textContent = article.title;
+			li.appendChild(link);
+			esportesLista.appendChild(li);
+		});
+	})
+	.catch(error => console.log(error));
+
+// Seção de entretenimento
+const entretenimentoLista = document.getElementById("ciencia-lista");
+
+fetch("https://newsapi.org/v2/top-headlines?country=br&category=science&apiKey=SUA_API_KEY")
+	.then(response => response.json())
+	.then(data => {
+		data.articles.forEach(article => {
+			const li = document.createElement("li");
+			const link = document.createElement("a");
+			link.href = article.url;
+			link.textContent = article.title;
+			li.appendChild(link);
+			entretenimentoLista.appendChild(li);
+		});
+	})
+	.catch(error => console.log(error));
+
+// Seção de negócios
+const negociosLista = document.getElementById("tecnologia-lista");
+
+fetch("https://newsapi.org/v2/top-headlines?country=br&category=technology&apiKey=SUA_API_KEY")
+	.then(response => response.json())
+	.then(data => {
+		data.articles.forEach(article => {
+			const li = document.createElement("li");
+			const link = document.createElement("a");
+			link.href = article.url;
+			link.textContent = article.title;
+			li.appendChild(link);
+			negociosLista.appendChild(li);
+		});
+	})
+	.catch(error => console.log(error));
