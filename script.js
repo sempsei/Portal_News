13b99ac6cc9ac083429e084716807904675602fb
Luan Rezende <flogsgameluan@gmail.com>
2023-03-07 01:57:11 -0300

    update

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const noticiasLista = document.getElementById("noticias-lista");
 
-fetch("https://newsapi.org/v2/top-headlines?country=br&apiKey=c4b913c495fe4138b231641539267631")
-	.then(response => response.json())
-	.then(data => {
-		data.articles.forEach(article => {
-			const li = document.createElement("li");
-			const link = document.createElement("a");
-			link.href = article.url;
-			link.textContent = article.title;
-			li.appendChild(link);
-			noticiasLista.appendChild(li);
-		});
-	})
-	.catch(error => console.log(error));
+fetch(
+  "https://newsapi.org/v2/top-headlines?country=br&apiKey=c4b913c495fe4138b231641539267631"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    data.articles.forEach((article) => {
+      const li = document.createElement("li");
+      const link = document.createElement("a");
+      link.href = article.url;
+      link.textContent = article.title;
+      li.appendChild(link);
+      noticiasLista.appendChild(li);
+    });
+  })
+  .catch((error) => console.log(error));
